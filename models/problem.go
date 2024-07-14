0067f4a33f4063e88043e83a0447969e16b60d2b
USTC-DingZhen <ycustc@mail.ustc.edu.cn>
2024-07-14 17:45:10 +0800

    update

--- models/problem.go
+++ models/problem.go
@@ -9,6 +9,8 @@ type Problem struct {
 	Identity   string `gorm:"column:identity;type:varchar(36);" json:"identity"` // 问题表的唯一标识
 	Title      string `gorm:"column:title;type:varchar(255);" json:"title"`      // 文章标题
 	Content    string `gorm:"column:content;type:text;" json:"content"`          // 文章正文
+	MaxRuntime int    `gorm:"column:max_runtime;type:int(11);" json:"max_runtime"`
+	MaxMemory  int    `gorm:"column:max_mem;type:int(11);" json:"max_memory"`
 	CategoryId string `gorm:"column:category_id;type:varchar(255);" json:"category_id"`
 }
 
